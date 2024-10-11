725c9ecd72d3c63f8acd11e696db53c86bffb302
slim arafa <slimarafa@gmail.com>
2024-10-11 10:58:40 +0100

    finalisation de d'authetification et de bouton logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+ 
+//garder les données d'authentification dans le service
+public username:any;
+public isAuthenticated:boolean=false;
+public roles:string[]=[];
   public users:any={
     admin:{password:'1234',roles:['STUDENTS','ADMIN']},
-    users1:{password:'12345',roles:['STUDENTS']},
+    user1:{password:'12345',roles:['STUDENTS']},
   }
-  constructor() { }
+  constructor(private router:Router) { }
 
   public login(username:string, password :string):boolean {
     if(this.users[username]&& this.users[username]['password']==password){
+      this.username=username;
+      this.isAuthenticated=true;
+      this.roles=this.users[username]['roles'];
       return true;
+    }else{
+
+      return false;
     }
-    return false;
+    
+  }
+   logout() {
+    this.username=undefined;
+    this.isAuthenticated=false;
+    this.roles=[]
+    this.router.navigateByUrl("/login")
   }
 }
